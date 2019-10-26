a4635ffde24d2284cdcabc1814f4531eaf3dd57f
TheEgid <egid.f1@gmail.com>
2019-10-26 16:59:59 +0300

    Add new fields

--- property/admin.py
+++ property/admin.py
@@ -4,6 +4,21 @@ from .models import Flat
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'town_district', 'address',)
-    readonly_fields = ['created_at',]
+    search_fields = ('town',
+                     'town_district',
+                     'address',
+                     )
+
+    readonly_fields = [
+        'created_at',
+    ]
+
+    list_display = ('address',
+                     'price',
+                     'new_building',
+                     'construction_year',
+                     'town'
+                     )
+
+    list_editable = ('new_building',)
 
